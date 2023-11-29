2d3f830ece8854e8ff91391a25000d15d7d325a3
Roman Mikaelyan <romanmik@yandex.ru>
2023-11-29 14:48:31 +0300

    Create the auto result page

--- src/pages/applying/ui/applying-result/ApplyingResult.tsx
+++ src/pages/applying/ui/applying-result/ApplyingResult.tsx
@@ -1,14 +1,21 @@
-import { FC } from 'react';
+import { FC } from "react";
 
+import styles from "./styles.module.scss";
+import { AppLink, AppLinkThemes } from "@/shared/ui/app-link";
 
 type ApplyingResultProps = {
   className?: string;
 };
 
-export const ApplyingResult:FC<ApplyingResultProps> = () => {
+export const ApplyingResult: FC<ApplyingResultProps> = () => {
   return (
-    <>
-      Result
-    </>
-  )
-}
+    <div className={styles.bb__applying_result}>
+      <div className={styles.bb__applying_result_icon}></div>
+      <h2>Заявка на рассмотрении</h2>
+      <p>В течении 20 мин. с вами свяжется наш агент</p>
+      <AppLink to="/" theme={AppLinkThemes.PRIMARY}>
+        Перейти в личный кабинет
+      </AppLink>
+    </div>
+  );
+};
