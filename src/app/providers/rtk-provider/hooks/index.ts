79e90aba349bd94edd01f037cdfbcd6eb59ca077
Roman Mikaelyan <romanmik@yandex.ru>
2024-01-15 14:02:23 +0300

    add types to the action creators

--- src/app/providers/rtk-provider/hooks/index.ts
+++ src/app/providers/rtk-provider/hooks/index.ts
@@ -1,13 +1,32 @@
 import { useMemo } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { ActionCreatorsMapObject, bindActionCreators } from "@reduxjs/toolkit";
+import {
+  ActionCreator,
+  ActionCreatorsMapObject,
+  AsyncThunk,
+  bindActionCreators,
+} from "@reduxjs/toolkit";
 
 import type { AppDispatch, RootState } from "../types";
 
 export const useAppDispatch = useDispatch<AppDispatch>;
 export const useStateSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const useActionCreators = (actions: ActionCreatorsMapObject) => {
+type BoundAsyncThunk<Action extends ActionCreator<any>> = (
+  ...args: Parameters<Action>
+) => ReturnType<ReturnType<Action>>;
+
+type BoundActions<Actions extends ActionCreatorsMapObject> = {
+  [key in keyof Actions]: Actions[key] extends AsyncThunk<any, any, any>
+    ? BoundAsyncThunk<Actions[key]>
+    : Actions[key];
+};
+
+export const useActionCreators = <
+  Actions extends ActionCreatorsMapObject = ActionCreatorsMapObject,
+>(
+  actions: Actions,
+): BoundActions<Actions> => {
   const dispatch = useAppDispatch();
 
   return useMemo(
