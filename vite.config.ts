0648e6d1275c894f395603119a92f110445d5901
Roman Mikaelyan <romanmik@yandex.ru>
2024-03-29 21:17:26 +0300

    preload the intro image

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import UnpluginInjectPreload from "unplugin-inject-preload/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    UnpluginInjectPreload({
+      injectTo: "head",
+      files: [
+        {
+          entryMatch: /.*\.(png|svg)$/,
+          outputMatch: /.*\.(png|svg)$/,
+        },
+      ],
+    }),
+  ],
   server: {
     host: true,
   },
